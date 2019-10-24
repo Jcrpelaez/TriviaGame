aea788e9f8f9163445a12a015d2389b75279a397
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-24 17:08:17 -0500

    added q&a's

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+$("#start").click(function() {
+for(var i= 0; i<questions.length;i++){
+    $("#subwrapper").append("<h2>"+questions[i].question+"</h2>");
+    for(var j=0;j<)
+    
+}
+});
+var questions = [{
+    question: "What was the name of Nintendo's first portable video game system?";
+    answers:["Game Boy","Nintendo DS","Virtual Boy","Game Boy Color"];
+    correctAnswer:"Game Boy"
+}, {
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    }, {
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    }, {
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    },{
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    },{
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    },{
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    },{
+        question: "What was the name of Nintendo's first portable video game system?";
+        answers: ["Game Boy", "Nintendo DS", "Virtual Boy", "Game Boy Color"];
+        correctAnswer: "Game Boy"
+    }];     
