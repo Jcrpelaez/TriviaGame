4aaaa2f1ac326ad1718d9066803973d28482457f
Juan Carlos <jcrpelaez@yahoo.com>
2019-10-31 16:46:36 -0500

    added visable countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,7 +57,10 @@ var game = {
   // created start game function
   start: function() {
     timer = setInterval(game.countdown, 1000);
-
+    $("#subwrapper").prepend(
+      "<h2>Time Remaining: <span id='counter'>120</span> Seconds</h2>"
+    );
+    $("#start").remove();
     for (var i = 0; i < questions.length; i++) {
       $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
       for (var j = 0; j < questions[i].answers.length; j++) {
